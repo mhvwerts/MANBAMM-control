809a8ce70df20ff227f45a5ea08a4fd301110178
Martinus Werts <martinus.werts@univ-angers.fr>
2024-12-01 12:55:22 +0100

    MOLTECH-FLSH flashbox, first commit

---
+++ MOLTECH-flashbox/README.md
@@ -0,0 +1,27 @@
+# MOLTECH-FLSH Flashbox
+
+*by Martinus Werts*
+
+*MOLTECH-Anjou, CNRS, Université d'Angers*
+
+The MOLTECH-FLSH Flashbox is a small USB-powered electronics box for generating precisely timed trigger and strobe TTL pulses in long-afterglow ('long-lifetime') photoluminescence ('phosphorescence') experiments (from 5 µs up to many seconds). The pulse timings are set via the USB serial connection of the Arduino. The pulse trains are essentially jitter-free down to a microsecond or so, thanks to tight coding.
+
+This section of the repository contains the hardware description of the MOLTECH-FLSH and the Arduino firmware. The host terminal software (Python programs running on the lab PC) can be found in `MANBAMM-control/python-src` together with the control software for other lab devices (*e.g.*, syringe pumps, fluidic valves, flowmeters).
+
+## Hardware
+
+In its present incarnation, the box is based on a recycled, ancient Arduino Duemilanove board (ATmega168) with the output pin (Arduino Pin 11, ATmega PB3) directly connected to a BNC terminal, without buffer or protection. The housing is a nice orange box. Other standard 5V Arduino boards (UNO and other ATmega328p boards) can be used without modification.
+
+![Photograph of the MOLTECH-FLSH Flashbox](./res/MOLTECH-FLSH-1small.jpg)
+
+
+## Firmware
+
+`firmware_flashbox_vX_X.ino` contains the current Arduino firmware. On one side, it takes care of switching the TTL outputs to generate the pulse sequences. On the other side, it interacts with the host terminal via a (USB) serial connection.
+
+The firmware code does not use the Arduino routines, but is vanilla AVR with direct manipulation of the ATmega registers, enabling very tight control of the timing and microsecond resolution. The code can still be used with the standard Arduino IDE for programming and uploading to the board.
+
+
+## USB serial communication protocol
+
+The protocol is minimalist, and uses ASCII text. Pending more documentation, the protocol can be easily deduced from the firmware source code.
---
+++ MOLTECH-flashbox/firmware_flashbox_v1_0/firmware_flashbox_v1_0.ino
@@ -0,0 +1,238 @@
+/*******************************************************************************
+ * Firmware for MOLTECH-Flashbox
+ *
+ * Generate TTL pulse trains of adjustable width, phase and period.
+ * Adjustment takes plase via USB/serial RS232-style interface.
+ *
+ * The MOLTECH-Flashbox has been designed for use with the digital phosphoroscope
+ * set-up
+ *
+ * This code targets the Arduino Duemilanove board with ATMega168 controller
+ * (recycle and reuse of old electronics!) but should work without modification 
+ * on any modern Arduino UNO with ATMega328p
+ *
+ * M. H. V. Werts, 2024
+ * MOLTECH-Anjou, CNRS, Université d'Angers
+ * France
+ ******************************************************************************/
+
+/* Changelog
+
+*/
+
+/* Some test results
+
+v241130-13h29 (firmwave v1.0)
+
+Period
+Value	                Period time (s)
+350000	              2.187
+320000	              2.000
+
+Pulse width
+Delta value (ON-OFF)	Pulse width (ms)
+32000	                200.0
+76000	                475.0
+78000	                487.5
+80000	                500.0
+
+
+v241130-11h03 (older version of firmware)
+  TIMINGS
+  cnt_period = 10000    period = 38.75 ms rock steady
+  cnt_on = 1000
+  cnt_off = 2000        pulse width = 3.876 ms rock solid    
+
+  cnt_period = 1000    period = 3.885 ms rock steady
+  cnt_on = 100
+  cnt_off = 200        pulse width = 0.3884 ms rock solid    
+
+                       rise time 7 ns (no load) SCOPE LIMITED
+                       fall time 7 ns (no load) SCOPE LIMITED
+
+*/
+
+// Hardware and firmware info - TODO: let device output these if requested
+const char *DEV_ID = "MOLTECH Flashbox 01"; // each device should have a unique identifier
+const char *FIRMWARE_VERSION = "1.0";
+
+
+#include <avr/io.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+// for USART_ReceiveDecimal
+#define MAX_DIGITS 10  // Max digits for a 32-bit signed integer
+
+// for state machine
+#define MAXSTATE 2
+#define FLASHING 0
+#define WAITING 1
+#define ERROR 2
+const char statechr[MAXSTATE+1] = "!.E";
+
+volatile unsigned char state;
+
+// for counter register
+// initial values give a 2s period with a 0.5s flash
+uint32_t cnt_on = 1000UL;
+uint32_t cnt_off = 81000UL; // delta = 80000
+uint32_t cnt_period = 320000UL;
+
+volatile uint32_t cnt;
+
+
+
+/* USART routines */
+
+void USART_Init(void) {
+  uint16_t ubrr_value = 8; // Baud rate 115200, 16 MHz clock
+  UBRR0H = (uint8_t)(ubrr_value >> 8);
+  UBRR0L = (uint8_t)(ubrr_value);
+  UCSR0B = (1 << RXEN0) | (1 << TXEN0); // Enable RX and TX
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // 8 data bits, 1 stop bit, no parity
+}
+
+void USART_Transmit(char data) {
+  while (!(UCSR0A & (1 << UDRE0))); // Wait for empty transmit buffer
+  UDR0 = data;
+}
+
+char USART_Receive(void) {
+  while (!(UCSR0A & (1 << RXC0))); // Wait for data to be received
+  return UDR0;
+}
+
+
+int32_t USART_ReceiveDecimal(void) {
+  int32_t result = 0;
+  uint8_t digit_count = 0;
+  char received_char;
+
+  while (1) {
+    // Wait for a character
+    while (!(UCSR0A & (1 << RXC0)))
+      ;
+    received_char = UDR0;  // Read the character
+
+    // Check if the character is a valid digit
+    if (received_char >= '0' && received_char <= '9') {
+      // Decode the digit and add it to the result
+      if (digit_count >= MAX_DIGITS) {
+        return -1; // Too many digits (integer overflow risk)
+      }
+
+      result = result * 10 + (received_char - '0');
+      digit_count++;
+    } 
+    // Check for the termination character 'd'
+    else if (received_char == 'd') {
+      return result; // Successfully decoded number
+    } 
+    // Invalid character
+    else {
+      return -1; // Error: Invalid character in input
+    }
+  }
+}
+
+
+
+
+
+/* main() loop */
+
+int main(void) {
+  char in_char; 
+  int32_t setvalue;
+
+  // Configure PB3 (Arduino Pin 11) as output
+  DDRB |= (1 << PB3);
+
+  // Set Pin 11 LOW
+  PORTB &= ~(1 << PB3);
+
+  // Initialize USART
+  USART_Init();
+
+  // reset counter register (32 bit unsigned)
+  cnt = 0;
+
+  state = FLASHING;
+
+  while(state <= MAXSTATE) {
+    if (state == FLASHING) {
+      if (cnt == cnt_on) {
+        // Set Pin 11 HIGH
+        PORTB |= (1 << PB3);
+      }
+      if (cnt == cnt_off) {
+        // Set Pin 11 LOW
+        PORTB &= ~(1 << PB3);
+      }
+      cnt++;
+      if (cnt == cnt_period) {
+        // Set Pin 11 LOW (not really necessary)
+        PORTB &= ~(1 << PB3);
+        cnt = 0;
+      }
+    }
+
+    // Poll for incoming data and change state or act
+    if (UCSR0A & (1 << RXC0)) {
+      in_char = UDR0; // Read the received character
+      switch (in_char) {
+        case '!':  
+          state = FLASHING;
+          break;
+
+        case '.':
+          state = WAITING;
+          break;
+
+        case '?':  // Echo a message
+          USART_Transmit(statechr[state]);
+          break;
+
+        case 'P':   // Set period
+          setvalue = USART_ReceiveDecimal();
+          if (setvalue > 0) {
+            cnt_period = (uint32_t) setvalue;
+            cnt = 0; // do not forget to reset counter when changing values!
+          }
+          else {
+            state = ERROR;
+          }
+          break;
+
+        case 'N':   // Set ON time
+          setvalue = USART_ReceiveDecimal();
+          if (setvalue > 0) {
+            cnt_on = (uint32_t) setvalue;
+            cnt = 0; // do not forget to reset counter when changing values!
+          }
+          else {
+            state = ERROR;
+          }
+          break;
+
+        case 'F':   // Set OFF time
+          setvalue = USART_ReceiveDecimal();
+          if (setvalue > 0) {
+            cnt_off = (uint32_t) setvalue;
+            cnt = 0; // do not forget to reset counter when changing values!
+          }
+          else {
+            state = ERROR;
+          }
+          break;
+
+        default:  // Unknown command
+          state = ERROR; // Set error indication
+          break;
+      }
+    }
+  }
+
+  return 0;
+}  
